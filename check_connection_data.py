eef3b45fd34192b4b1abe4624aec2d9d4a5405d7
brunodev12 <bruno.carthery12@gmail.com>
2024-08-07 00:14:15 -0300

    feat: added blast network

--- check_connection_data.py
+++ check_connection_data.py
@@ -3,11 +3,18 @@ from data.constants import address
 from utils.helpers import days_counter, remove_expired_items
 
 
-if __name__ == '__main__':
+def removeExpiredItemsCD(address:str, chain:str):
 
-    connection_data = db.getConnectionData(address)
+    connection_data = db.getConnectionData(address, chain)
 
+    print(f"---------------{chain.upper()}---------------")
     days_counter(connection_data)
     connection_data_expired = remove_expired_items(connection_data, 30)
     
-    db.removeExpiredConnectionData(connection_data_expired)
+    db.removeExpiredConnectionData(connection_data_expired, chain)
+
+
+if __name__ == '__main__':
+
+    removeExpiredItemsCD(address, 'ethereum')
+    removeExpiredItemsCD(address, 'blast')
